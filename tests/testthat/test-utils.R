99e92070643e92e434b8f5af0edf6e6255171c04
Stathis Gennatas <gennatas@gmail.com>
2025-08-11 15:06:40 -0700

    use cells

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,10 +1,12 @@
+# test-utils.R
+# ::rtemisbio::
 
-# gene2sequence
+# %% gene2sequence ----
 test_that("gene2sequence works", {
   expect_s3_class(gene2sequence("MAPT"), "data.frame")
 })
 
-# aa_sub
+# %% aa_sub ----
 test_that("aa_sub works", {
   expect_equal(aa_sub("ACDEFG", c("C2A")), c("A", "A", "D", "E", "F", "G"))
 })
