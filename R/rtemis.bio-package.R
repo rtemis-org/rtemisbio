9484a295adcb0db4e1e742b495d41f7795006204
Stathis Gennatas <gennatas@gmail.com>
2026-02-14 10:55:45 -0800

    init pkglogo

--- R/rtemis.bio-package.R
+++ R/rtemis.bio-package.R
@@ -2,6 +2,30 @@
 # ::rtemis.bio::
 # 2026 EDG rtemis.org
 
+pkglogo <- function(
+  pkg,
+  filename = paste0(pkg, ".utf8"),
+  fmt_fn = color_txt_columns,
+  args = list(
+    color_left = rtemis_colors[["blue"]],
+    color_right = rtemis_colors[["green"]],
+    output_type = "ansi"
+  ),
+  pad = 0L
+) {
+  logo_file <- system.file(
+    package = .packageName,
+    "resources",
+    filename
+  )
+  logo_txt <- readLines(logo_file)
+  paste0(
+    strrep(" ", pad),
+    do.call(fmt_fn, c(list(x = logo_txt), args)),
+    collapse = "\n"
+  )
+} # /rtemis::pkglogo
+
 #' @name rtemis.bio-package
 #'
 #' @title rtemis.bio: Bioinformatics ops
